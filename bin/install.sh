c84aa5f4ddbfe9e50c4d6fe42d1ae4b6e57cf3d4
kobanyan <kobanyan@users.noreply.github.com>
2017-01-21 00:51:35 +0900

    Move utilities to main shell

--- bin/install.sh
+++ bin/install.sh
@@ -3,8 +3,6 @@
 DOTFILES_REPO="https://github.com/kobanyan/dotfiles"
 DOTFILES_HOME="$HOME/src/github.com/kobanyan/dotfiles"
 
-# precheck
-
 REQUIRED_PACKAGES_MAC=(
   curl
   git
@@ -18,6 +16,28 @@ REQUIRED_PACKAGES_LINUX=(
   python-software-properties
 )
 
+# -----------------------------------------------------------------------------
+
+COLOR_ERROR="\033[1;31m"
+COLOR_WARN="\033[1;33m"
+COLOR_RESET="\033[0m"
+
+function now() {
+  date "+%Y-%m-%d %H:%M:%S"
+}
+
+function log_info() {
+  echo -e "`now` [INFO] $@"
+}
+
+function log_warn() {
+  echo -e "$COLOR_WARN`now` [WARN] $@$COLOR_RESET"
+}
+
+function log_error() {
+  echo -e "$COLOR_ERROR`now` [ERROR] $@$COLOR_RESET"
+}
+
 function check_if_do_preinstall() {
   local packages=($@)
   for p in ${packages[@]}; do
@@ -27,32 +47,32 @@ function check_if_do_preinstall() {
   done
 }
 
-echo "Starting precheck..."
+# -----------------------------------------------------------------------------
+
+log_info "Starting precheck..."
 case `uname` in
   "Darwin" )
     check_if_do_preinstall ${REQUIRED_PACKAGES_MAC[@]}
     if [ "$do_preinstall" == "true" ]; then
-      echo "Required packages do not exist! Can not install!"
+      log_error "Required packages do not exist! Can not install!"
       exit 1
     fi;;
   "Linux"  )
     check_if_do_preinstall ${REQUIRED_PACKAGES_LINUX[@]}
     if [ "$do_preinstall" == "true" ]; then
-      echo "Installing required packages with package manager..."
+      log_info "Installing required packages with package manager..."
       sudo apt update
       sudo apt install -y  ${REQUIRED_PACKAGES_LINUX[*]}
     fi;;
 esac
-echo "Finished precheck."
+log_info "Finished precheck."
 
-echo "Cloning $DOTFILES_REPO to $DOTFILES_HOME"
+log_info "Cloning $DOTFILES_REPO to $DOTFILES_HOME"
 if [ -d "$DOTFILES_HOME/.git" ] || git -C $DOTFILES_HOME rev-parse --git-dir >/dev/null 2>&1; then
   git -C $DOTFILES_HOME pull
 else
   git clone $DOTFILES_REPO $DOTFILES_HOME
 fi
-
-source "$DOTFILES_HOME/lib/utils.sh"
 
 case `uname` in
   "Darwin" )
