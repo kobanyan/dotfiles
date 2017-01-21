dc1679fe41170e28702676e9558475f377c50062
kobanyan <kobanyan@users.noreply.github.com>
2017-01-21 15:39:02 +0900

    Enabel to install code extensions

--- bin/install.sh
+++ bin/install.sh
@@ -90,6 +90,7 @@ esac
 
 source "$DOTFILES_HOME/lib/fonts.sh"
 source "$DOTFILES_HOME/lib/fisher.sh"
+source "$DOTFILES_HOME/lib/code.sh"
 source "$DOTFILES_HOME/lib/dotfiles.sh"
 
 case `uname` in
---
+++ lib/code.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+log_info "Installing code extensions..."
+
+code --install-extension Shan.code-settings-sync
