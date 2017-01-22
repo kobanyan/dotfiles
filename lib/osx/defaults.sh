052c68765a1c92a9f8ea1a10aef1ae330f4174fb
kobanyan <kobanyan@users.noreply.github.com>
2017-01-22 15:52:38 +0900

    Enable to run all defaults files

--- lib/osx/defaults.sh
+++ lib/osx/defaults.sh
@@ -2,7 +2,7 @@
 
 log_info "Installing preferences using defaults..."
 
-for file in "${DOTFILES_HOME:?}/var/osx/defaults/*"
+for file in ${DOTFILES_HOME:?}/var/osx/defaults/*
 do
   source $file
 done
