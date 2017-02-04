2aa02015dca002e18ce2fa78ef4705cf0b3df055
kobanyan <kobanyan@users.noreply.github.com>
2017-02-04 15:37:00 +0900

    Re-arrange config.fish

--- var/home/.config/fish/config.fish
+++ var/home/.config/fish/config.fish
@@ -1,15 +1,11 @@
-# test (cat /proc/sys/kernel/osrelease | grep Microsoft)
-
-# set -x GOPATH "$HOME/.go"
-# set -x PATH $PATH $GOPATH/bin
+# pyenv
 set -x PATH "$HOME/.pyenv/bin" $PATH
 status --is-interactive;
   and source (pyenv init - | psub);
     and source (pyenv virtualenv-init - | psub)
 
-# gi update-completions &
+# gbs
+set -g GBS_SELECTOR fzf
+
 # theme
 set -g BULLETTRAIN_NODEJS_SHOW true
-# set -g BULLETTRAIN_GO_SHOW true
-
-set -g GBS_SELECTOR fzf
