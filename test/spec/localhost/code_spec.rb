c062d6ad748de48b09d790b210cafb5cd05b3995
kobanyan <kobanyan@users.noreply.github.com>
2017-02-06 17:28:13 +0900

    Fix bug about illegal method name

--- test/spec/localhost/code_spec.rb
+++ test/spec/localhost/code_spec.rb
@@ -1,6 +1,6 @@
 require "spec_helper"
 
-if os[:family] == "darwin" || (["ubuntu"].include?(os[:family]) && ubuntu_has_desktop)
+if os[:family] == "darwin" || (["ubuntu"].include?(os[:family]) && has_ubuntu_desktop)
   describe command("code --list-extensions | grep -q 'Shan.code-settings-sync'") do
     its(:exit_status) { should eq 0 }
   end
