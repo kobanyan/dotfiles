39d2bb78f3796958562b49331fb448d06e61b822
kobanyan <kobanyan@users.noreply.github.com>
2018-11-06 01:03:25 +0900

    Use latest ruby

--- lib/rbenv.sh
+++ lib/rbenv.sh
@@ -7,7 +7,7 @@ git_clone_or_pull https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-
 
 export PATH="$HOME/.rbenv/bin:$PATH"
 eval "$(rbenv init -)"
-rbenv versions | grep 2.2.5 >/dev/null 2>&1
+rbenv versions | grep 2.5.3 >/dev/null 2>&1
 if [ $? != 0 ]; then
-  rbenv install 2.2.5
+  rbenv install 2.5.3
 fi
