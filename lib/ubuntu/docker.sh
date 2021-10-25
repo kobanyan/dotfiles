c606e6001915111aab7dfd17868a437a8317c9eb
kobanyan <kobanyan@users.noreply.github.com>
2021-10-25 17:06:42 +0900

    silent curl

--- lib/ubuntu/docker.sh
+++ lib/ubuntu/docker.sh
@@ -12,5 +12,5 @@ bash -c "$(curl -fsSL https://get.docker.com)"
 log_info "Installing docker compose..."
 
 mkdir -p $HOME/.docker/cli-plugins/
-curl -SL https://github.com/docker/compose/releases/download/v2.0.1/docker-compose-linux-x86_64 -o $HOME/.docker/cli-plugins/docker-compose
+curl -sSL https://github.com/docker/compose/releases/download/v2.0.1/docker-compose-linux-x86_64 -o $HOME/.docker/cli-plugins/docker-compose
 chmod +x $HOME/.docker/cli-plugins/docker-compose
