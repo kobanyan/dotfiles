07bdf3290a1e24a3bb692391875c4030b4b7f54b
kobanyan <kobanyan@users.noreply.github.com>
2021-10-25 17:27:56 +0900

    fix test

--- test/spec/localhost/asdf_spec.rb
+++ test/spec/localhost/asdf_spec.rb
@@ -1,21 +1,21 @@
 require "spec_helper"
 
-describe command("fish -C 'asdf --version'") do
+describe command("fish -c 'source ~/.asdf/asdf.fish; and asdf --version'") do
   its(:exit_status) { should eq 0 }
 end
 
-describe command("fish -C 'asdf current nodejs | grep -q '14.17.2''") do
+describe command("fish -c 'source ~/.asdf/asdf.fish; and asdf current nodejs | grep -q '14.17.2''") do
   its(:exit_status) { should eq 0 }
 end
 
-describe command("fish -C 'asdf current ruby | grep -q '3.0.2''") do
+describe command("fish -c 'source ~/.asdf/asdf.fish; and asdf current ruby | grep -q '3.0.2''") do
   its(:exit_status) { should eq 0 }
 end
 
-describe command("fish -C 'asdf current python | grep -q '3.9.7''") do
+describe command("fish -c 'source ~/.asdf/asdf.fish; and asdf current python | grep -q '3.9.7''") do
   its(:exit_status) { should eq 0 }
 end
 
-describe command("fish -C 'pipenv --version'") do
+describe command("fish -c 'source ~/.asdf/asdf.fish; and pipenv --version'") do
   its(:exit_status) { should eq 0 }
 end
