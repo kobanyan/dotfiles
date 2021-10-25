db6090f212a4ce974e8f477ffd297b93e5c35f10
kobanyan <kobanyan@users.noreply.github.com>
2021-10-25 17:03:07 +0900

    fix prompt config

--- var/home/.config/fish/config.fish
+++ var/home/.config/fish/config.fish
@@ -6,7 +6,7 @@ set PATH ~/.local/bin $PATH
 
 # theme
 set -U tide_left_prompt_items pwd git newline character
-set -U status cmd_duration context jobs asdf_nodejs asdf_ruby asdf_python asdf_golang time
+set -U tide_right_prompt_items status cmd_duration context jobs asdf_nodejs asdf_ruby asdf_python asdf_golang time
 
 # americanhanko/fish-spin
 set -U XDG_CACHE_HOME $HOME/.cache
