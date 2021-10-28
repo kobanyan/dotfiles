364dd8fc5f976fee8c4c7a4cc847d207ffc8e13f
kobanyan <kobanyan@users.noreply.github.com>
2021-10-28 17:29:44 +0900

    install asdf with homebrew

--- lib/asdf.sh
+++ lib/asdf.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
 
-log_info "Installing asdf..."
+log_info "Installing asdf plugins..."
 
-ASDF_HOME=$HOME/.asdf
-if [ ! -d "$ASDF_HOME/.git" ]; then
-  git clone https://github.com/asdf-vm/asdf.git $ASDF_HOME
-fi
-git -C $ASDF_HOME fetch -p
-git -C $ASDF_HOME checkout v0.8.1
-
-if [ ! -e "$HOME/.config/fish/completions/asdf.fish" ]; then
-  mkdir -p $HOME/.config/fish/completions
-  ln -s $ASDF_HOME/completions/asdf.fish $HOME/.config/fish/completions
-fi
-source $ASDF_HOME/asdf.sh
+source $(brew --prefix asdf)/libexec/asdf.sh
 
 log_info "Installing direnv..."
 asdf plugin-add direnv
@@ -22,7 +11,6 @@ asdf global direnv latest
 
 log_info "Installing nodejs..."
 asdf plugin-add nodejs
-bash $ASDF_HOME/plugins/nodejs/bin/import-release-team-keyring
 asdf install nodejs 14.17.2
 asdf global nodejs 14.17.2
 
@@ -32,8 +20,8 @@ asdf install ruby 3.0.2
 asdf global ruby 3.0.2
 
 log_info "Installing python..."
-# https://pipenv-ja.readthedocs.io/ja/translate-ja/install.html#installing-pipenv
-asdf plugin-add python https://github.com/tuvistavie/asdf-python.git
+asdf plugin-add python
 asdf install python 3.9.7
 asdf global python 3.9.7
+# https://pipenv-ja.readthedocs.io/ja/translate-ja/install.html#installing-pipenv
 pip install --user pipenv
--- var/homebrew.sh
+++ var/homebrew.sh
@@ -3,6 +3,7 @@ HOMEBREW_URL="https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
 TAPS=(
 )
 BOTTLES=(
+  asdf
   awscli
   docker-completion
   fish
